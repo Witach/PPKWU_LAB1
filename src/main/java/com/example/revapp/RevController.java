9083e52c6168c7d4ee415e3076f10a1d3c826685
witaszek98@wp.pl <witaszek98@wp.pl>
2020-10-20 14:56:18 +0200

    RevController HTTP verb changed into GET

--- src/main/java/com/example/revapp/RevController.java
+++ src/main/java/com/example/revapp/RevController.java
@@ -1,6 +1,7 @@
 package com.example.revapp;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RevController {
 
-    @PostMapping("/rev")
+    @GetMapping("/rev")
     ResponseEntity<TextDto> reverseText(@RequestBody TextDto textDto) {
         var reversedText = new StringBuilder()
                 .append(textDto.getContent())
