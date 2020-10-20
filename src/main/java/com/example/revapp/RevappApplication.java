880b850537c1719fe30eb5678159481eb27741ac
witaszek98@wp.pl <witaszek98@wp.pl>
2020-10-20 14:26:47 +0200

    spring boot project initialized

---
+++ src/main/java/com/example/revapp/RevappApplication.java
@@ -0,0 +1,13 @@
+package com.example.revapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/revapp/RevappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.revapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
